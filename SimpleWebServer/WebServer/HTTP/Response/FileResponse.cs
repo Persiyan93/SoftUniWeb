dd975278981fceee683153e7977b77729db0f52a
Persiyan93 <persiyan.georgiev@gmail.com>
2021-03-23 13:17:04 +0200

    Get FavIcon
    Return Fav Icon

--- SimpleWebServer/WebServer/HTTP/Response/FileResponse.cs
+++ SimpleWebServer/WebServer/HTTP/Response/FileResponse.cs
@@ -11,6 +11,10 @@ namespace HTTP.Response
         public FileResponse(byte[] fileContent,string contentType)
         {
 
+            this.responseCode = HttpResponseCode.Ok;
+            this.Body = fileContent;
+            this.Headers.Add(new Header("Content-Type", contentType));
+            this.Headers.Add(new Header("Content-Length ", this.Body.Length.ToString()));
         }
     }
 }
--- SimpleWebServer/WebServer/TestHttp/Program.cs
+++ SimpleWebServer/WebServer/TestHttp/Program.cs
@@ -2,6 +2,7 @@
 using HTTP.Response;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Text;
 
 namespace TestHttp
@@ -14,10 +15,17 @@ namespace TestHttp
             routeTable.Add(new Route("/", HttpMethodType.Get, Index));
             routeTable.Add(new Route("/users/Login", HttpMethodType.Get, Login));
             routeTable.Add(new Route("/users/Login", HttpMethodType.Post, DoLogin));
+            routeTable.Add(new Route("/favicon.ico", HttpMethodType.Get, GetFavIcon));
             HttpServer httpServer = new HttpServer(80,routeTable);
            
             httpServer.StratAsync().GetAwaiter().GetResult();
 
+        }
+        public static HttpResponse GetFavIcon(HttpRequest request)
+        {
+            var favIconByte = File.ReadAllBytes("wwwroot/favicon.ico");
+            
+            return new FileResponse(favIconByte, "image / x - icon");
         }
         public static HttpResponse Index(HttpRequest request)
         {
@@ -26,7 +34,13 @@ namespace TestHttp
         }
         public static HttpResponse Login(HttpRequest request)
         {
-            return new HtmlResponse("<h1>Login page</h1>");
+            if (!request.SessionData.ContainsKey("UserName"))
+            {
+                request.SessionData ["Username"] = "Pesho";
+            }
+
+            var response = new HtmlResponse($"<h1>Hello {request.SessionData["Username"]} page</h1>");
+            return response;
           
         }
         public static HttpResponse DoLogin(HttpRequest request)
